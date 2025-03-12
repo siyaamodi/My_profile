31af84127f603202a7cc06cb4710a3f91448e094
siyaamodi <kumarisiya034@gmail.com>
2025-03-12 14:39:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function About()
+{
+document.getElementById("about_me").scrollIntoView({behavior:"smooth"})
+}
+function Services()
+{
+    document.getElementById("services").scrollIntoView({behavior:"smooth"})
+}
+function Contact()
+{
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"})
+}
